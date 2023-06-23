0661c21cb089f02153b862130fca6ba681870ead
Nicolás Vaquero <nicolasvaquero96@gmail.com>
2023-06-23 19:11:48 -0300

    primer commit, anadimos el routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout/Layout";
+import { routes } from "./Components/utils/routes";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Favs from "./Routes/Favs";
+import PageNotFound from "./Routes/PageNotFound";
 
 
 function App() {
   return (
       <div className="App">
-          <Navbar/>
-          <Footer/>
+        <Routes>
+          <Route path='/' element={<Layout/>}>
+            <Route path={routes.home} element={<Home/>}/>
+            <Route path={routes.contact} element={<Contact/>}/>
+            <Route path={routes.favs} element={<Favs/>}/>
+            <Route path={routes.pageNotFound} element={<PageNotFound/>}/>                 
+          </Route>
+        </Routes>
       </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 
+import { Link, navigate } from 'react-router-dom'
+import { routes } from './utils/routes'
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
 
   return (
     <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+          <div className='links'>
+            {/* <button onClick={() => navigate(-1)}>↩Back</button> */}
+              <Link to={routes.home}><h4 className='link'>Home</h4></Link>
+              <Link to={routes.contact}><h4 className='link'>Contact</h4></Link>
+              <Link to={routes.favs}><h4 className='link'>Favs</h4></Link>
+          </div>
+{/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+{/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
+          {/* <button>Change theme</button> */}
     </nav>
   )
 }
