cfcc435ca63b097189617756968ba356c28c285d
MauroLucero <mauro.luceror@gmail.com>
2023-06-23 20:54:52 -0300

    Card map implemented

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-
+import doctor from "..";
 
 const Card = ({ name, username, id }) => {
-
-  const addFav = ()=>{
+  const addFav = () => {
     // Aqui iria la logica para agregar la Card en el localStorage
-  }
+  };
 
   return (
     <div className="card">
-        {/* En cada card deberan mostrar en name - username y el id */}
+      {/* En cada card deberan mostrar en name - username y el id */}
+      <img src={"./images/doctor.jpg"} alt="doctor-logo" />
+      <h2>{name}</h2>
+      <p>{username}</p>
+      {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
-        {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
-
-        {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
+      {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
+      <button onClick={addFav} className="favButton">
+        Add fav
+      </button>
     </div>
   );
 };
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
-import Card from '../Components/Card'
+import React from "react";
+import Card from "../Components/Card";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { response } from "msw";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
+  const url = "https://jsonplaceholder.typicode.com/users/";
+
+  const [doctores, setDoctores] = useState([{}]);
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setDoctores(response.data);
+    });
+  }, []);
+
+  console.log(doctores);
+
   return (
-    <main className="" >
+    <main className="">
       <h1>Home</h1>
-      <div className='card-grid'>
-        {/* Aqui deberias renderizar las cards */}
+      <div className="card-grid">
+        {doctores.map(function (doctor) {
+          return <Card name={doctor.name} username={doctor.username} />;
+        })}
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
