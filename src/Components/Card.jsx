8817e71f031021ee0a40fc95f552003d05852a5a
MauroLucero <mauro.luceror@gmail.com>
2023-06-30 17:00:12 -0300

    minor fixes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -20,12 +20,13 @@ const Card = ({ name, username, id }) => {
 
   return (
     <div className="card">
+      {/* En cada card deberan mostrar en name - username y el id */}
+      <img src={"./images/doctor.jpg"} alt="doctor-logo" className="images" />
       <Link to={"/detail/" + id}>
-        {/* En cada card deberan mostrar en name - username y el id */}
-        <img src={"./images/doctor.jpg"} alt="doctor-logo" className="images" />
         <h3>{name}</h3>
-        <p>{username}</p>
       </Link>
+      <p>{username}</p>
+
       {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
       {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
